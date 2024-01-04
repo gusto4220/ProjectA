2511fd03df9f8a1c67063187c5bc6ed9e12346ef
gusto4220 <155581855+gusto4220@users.noreply.github.com>
2024-01-03 20:39:55 -0600

    Update DemoA.java

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -10,5 +10,8 @@ public class DemoA {
 	public static void add(int a, int b) {
 	System.out.println(a+b);
 	}
-
+	//Coming From GitHUB
+	public static void subtract(int a, int b) {
+	System.out.println(a-b);
+	}
 }
